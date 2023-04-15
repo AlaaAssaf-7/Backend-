7c5a7ce85130015edd591f26d308a6a839c7389a
Alosh95 <alaaassaf120@gmail.com>
2023-04-15 02:50:01 +0200

    HHHH

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -15,6 +15,13 @@ module.exports = function (
     app.get('/', function (req, res) { 
         res.status(200).json({name: "alaa"})
     })
+
+    app.get('/position', function(req, res){
+        res.status(200).json( { 
+            longitude: "11.232.445.34",
+            latitude: "232.22.545.2"
+        })
+    })
     return app; 
 
 }
